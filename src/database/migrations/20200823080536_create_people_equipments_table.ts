bdaf64b3a854e6817f0021e242d989596d585441
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-23 08:37:45 -0300

    Changed people-equipments relationship to include quantity

--- src/database/migrations/20200823080536_create_people_equipments_table.ts
+++ src/database/migrations/20200823080536_create_people_equipments_table.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary();
     table.uuid('person_id').notNullable();
     table.uuid('equipment_id').notNullable();
+    table.integer('quantity').defaultTo(0).notNullable();
 
     table
       .foreign('person_id')
