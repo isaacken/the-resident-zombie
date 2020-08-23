179231585bb88dbeb3e8bf653d299146bb1d81e2
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-23 05:38:32 -0300

    Fix people migration

--- src/database/migrations/20200822231714_create_people_table.ts
+++ src/database/migrations/20200822231714_create_people_table.ts
@@ -3,7 +3,7 @@ import { v4 as uuid } from 'uuid';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('people', table => {
-    table.uuid('id').notNullable().defaultTo(uuid()).primary();
+    table.uuid('id').notNullable().primary();
     table.string('name').notNullable();
     table.integer('age').notNullable();
     table.enum('gender', ['M', 'F']).notNullable();
