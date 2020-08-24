f64d0c5363057b86ef7ad677f6c23d6b049d3d42
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-24 02:12:45 -0300

    Created store method in Person controller

--- src/app/controllers/PersonController.ts
+++ src/app/controllers/PersonController.ts
@@ -2,23 +2,51 @@ import { Response, Request } from "express";
 import { v4 as uuid } from 'uuid';
 
 import PersonValidator from "../services/validators/PersonValidator";
+import EquipmentValidator from "../services/validators/EquipmentValidator";
 import db from "../../config/database";
-import Person from "../models/Person";
+import Person from "../entities/Person";
+import Equipment from "../entities/Equipment";
 
 class PersonController {
   async store(req: Request, res: Response) {
     const data = req.body;
 
+    let personData = new Person(data.person);
+
+    let inventoryData = data.inventory;
+
     try {
       data.id = uuid();
-      const personValidator = new PersonValidator(data);
-      personValidator.validateRegister();
+      const personValidator = new PersonValidator(personData);
+      await personValidator.validateRegister();
+      
+      const equipmentValidator = new EquipmentValidator(inventoryData);
+      await equipmentValidator.validateRegister();
     } catch (exception) {
-      console.log(exception);
       return res.status(400).json(exception);
     }
+    
+    let person: Person | any;
+     
+    try {
+      let trx = await db.transaction();
+      person = (await trx('people').returning('*').insert(personData))[0];
 
-    const person = await Person.insert(data);
+      for (let i in inventoryData) {
+        let equipment: Equipment = await db('equipments').where('item_name', 'Fiji Water').first();
+
+        inventoryData[i].id = uuid();
+        inventoryData[i].person_id = person.id;
+        inventoryData[i].equipment_id = equipment.id;
+        delete inventoryData[i].item_name;
+      }
+  
+      await trx('people_equipments').insert(inventoryData);
+  
+      trx.commit();
+    } catch (ex) {
+      return res.status(500).json({ message: 'internal server error' });
+    }
 
     return res.status(201).json(person);
   }
